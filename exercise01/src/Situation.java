c0e5bfc117afd64595cf170efb01ef3dcd795ddf
millinma <milling.manuel@gmail.com>
2017-05-23 13:36:02 +0200

    Added class Situation

---
+++ exercise01/src/Situation.java
@@ -0,0 +1,66 @@
+import bwapi.Unit;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Severin Wünsch on 22.05.17.
+ */
+public class Situation {
+
+    public static final int alliesArraySize = 5;
+    public static final int enemiesArraySize = 5;
+    public static final int maxUnits = 50;
+    public static final int maxPosX = 1000;
+    public static final int maxPosY = 1000;
+
+
+    private double unitPosX;
+    private double unitPosY;
+
+
+
+    public Situation(Unit unit){
+        int numberAlliesInSight = getNumberAlliesInSight();
+        int numberEnemiesOnMap = getNumberEnemiesInSight();
+        int killCountEnemies = getKillCountEnemies();
+        int killCountAllies = getKillCountAllies();
+
+        ArrayList<Unit> enemiesInSight = new ArrayList<Unit>();
+        enemiesInSight = getEnemiesInSight();
+        ArrayList<Unit> alliesInSight = new ArrayList<Unit>();
+        alliesInSight = getAlliesInSight();
+    }
+
+    private ArrayList<Unit> getAlliesInSight() {
+        return null;
+    }
+
+    private ArrayList<Unit> getEnemiesInSight() {
+        return null;
+    }
+
+    private int getKillCountAllies() {
+        return 0;
+    }
+
+    private int getKillCountEnemies() {
+        return 0;
+    }
+
+    private int getNumberEnemiesInSight() {
+        return 0;
+    }
+
+    private int getNumberAlliesInSight() {
+        return 0;
+    }
+    //private Pattern conditionPattern;
+
+    //public Condition(String regex) {
+      //  conditionPattern = Pattern.compile(regex);
+    //}
+
+    //public boolean matches(String environment) {
+    //    return conditionPattern.matcher(environment).matches();
+    //}
+}
